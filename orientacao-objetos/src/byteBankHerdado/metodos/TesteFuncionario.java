66219a01a8b11af437e21f67ddb00d09759b9aad
Sabine Hernandes <sabinehernandes@gmail.com>
2021-06-22 20:34:17 -0300

    Atualização do código devido à inclusão da classe abstrata

--- orientacao-objetos/src/byteBankHerdado/metodos/TesteFuncionario.java
+++ orientacao-objetos/src/byteBankHerdado/metodos/TesteFuncionario.java
@@ -1,12 +1,15 @@
 package byteBankHerdado.metodos;
 
 import byteBankHerdado.classes.Funcionario;
+import byteBankHerdado.classes.Gerente;
 
 public class TesteFuncionario {
 
     public static void main(String[] args) {
 
-        Funcionario f1 = new Funcionario();
+//        Não é mais possível criar um funcionário pois a classe é abstrata
+//        Funcionario f1 = new Funcionario();
+        Funcionario f1 = new Gerente();
         f1.setNome("Sabine Hernandes");
         f1.setCpf("000.000.000-00");
         f1.setSalario(3000);
--- orientacao-objetos/src/byteBankHerdado/metodos/TesteReferencias.java
+++ orientacao-objetos/src/byteBankHerdado/metodos/TesteReferencias.java
@@ -1,40 +1,42 @@
 package byteBankHerdado.metodos;
 
-import byteBankHerdado.classes.ControleBonificacao;
-import byteBankHerdado.classes.EditorVideo;
-import byteBankHerdado.classes.Funcionario;
-import byteBankHerdado.classes.Gerente;
+import byteBankHerdado.classes.*;
 
 public class TesteReferencias {
 
     public static void main(String[] args) {
 
+//        Não é mais possível criar um funcionário pois a classe é abstrata
+//        Funcionario funcionario1 = new Funcionario();
+//        funcionario1.setNome("Luciana");
+//        funcionario1.setSalario(2000.0);
+
         Gerente gerente1 = new Gerente();
         gerente1.setNome("Juliana");
         gerente1.setSalario(5000.0);
 
-        Funcionario funcionario1 = new Funcionario();
-        funcionario1.setNome("Luciana");
-        funcionario1.setSalario(2000.0);
+        Designer designer1 = new Designer();
+        designer1.setNome("Jessica");
+        designer1.setSalario(2000.0);
 
         EditorVideo editorvideo1 = new EditorVideo();
         editorvideo1.setNome("Vitor");
         editorvideo1.setSalario(2500.0);
 
         ControleBonificacao controle = new ControleBonificacao();
-        //Bonificação gerente = 5250
+        //Bonificação gerente = 5000
         controle.registra(gerente1);
         System.out.println("O bônus do gerente é de R$" + gerente1.getBonificacao());
 
-        //Bonificação funcionário = 100
-        controle.registra(funcionario1);
-        System.out.println("O bônus do funcionário é de R$" + funcionario1.getBonificacao());
-
-        //Bonificação editor = 225
+        //Bonificação editor = 150
         controle.registra(editorvideo1);
         System.out.println("O bônus do editor de vídeo é de R$" + editorvideo1.getBonificacao());
 
-        //Soma dos bônus = 5575
+        //Bonificação designer = 200
+        controle.registra(designer1);
+        System.out.println("O bônus do designer é de R$" + designer1.getBonificacao());;
+
+        //Soma dos bônus = 5350
         System.out.println("A soma de todos os bônus é de R$" + controle.getSoma());
 
     }
