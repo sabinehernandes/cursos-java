02731c562803f849669c327e581884a78966caa6
Sabine Hernandes <sabinehernandes@gmail.com>
2021-06-22 20:38:22 -0300

    Criação do sistema para autenticação de senha

---
+++ orientacao-objetos/src/byteBankHerdado/classes/SistemaInterno.java
@@ -0,0 +1,15 @@
+package byteBankHerdado.classes;
+
+public class SistemaInterno {
+
+    private int senha = 2222;
+
+    public void autentica(FuncionarioAutenticavel fa1) {
+        boolean autenticou = fa1.autentica(this.senha);
+        if (autenticou) {
+            System.out.println("Senha correta");
+        } else {
+            System.out.println("Senha incorreta");
+        }
+    }
+}
