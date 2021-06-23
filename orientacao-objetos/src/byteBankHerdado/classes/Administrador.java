6cb17f0d9ea11f56a1f24f4eb5f0dba0453faed6
Sabine Hernandes <sabinehernandes@gmail.com>
2021-06-23 11:21:00 -0300

    Atualização da extensão da herança e implementação da interface

--- orientacao-objetos/src/byteBankHerdado/classes/Administrador.java
+++ orientacao-objetos/src/byteBankHerdado/classes/Administrador.java
@@ -1,10 +1,27 @@
 package byteBankHerdado.classes;
 
-public class Administrador extends FuncionarioAutenticavel {
+public class Administrador extends Funcionario implements Autenticavel {
+
+    private AutenticacaoUtil autenticador;
+
+    public Administrador() {
+        this.autenticador = new AutenticacaoUtil();
+    }
 
     @Override
     public double getBonificacao() {
         return 50;
     }
 
+    @Override
+    public void setSenha(int senha) {
+        this.autenticador.setSenha(senha);
+
+    }
+
+    @Override
+    public boolean autentica(int senha) {
+        return this.autenticador.autentica(senha);
+    }
 }
+
--- orientacao-objetos/src/byteBankHerdado/classes/Gerente.java
+++ orientacao-objetos/src/byteBankHerdado/classes/Gerente.java
@@ -1,10 +1,26 @@
 package byteBankHerdado.classes;
 
 //Gerente herda da classe Funcionario
-public class Gerente extends FuncionarioAutenticavel {
+public class Gerente extends Funcionario implements Autenticavel {
 
-        public double getBonificacao() {
-            return super.getSalario();
+    private AutenticacaoUtil autenticador;
+
+    public Gerente() {
+        this.autenticador = new AutenticacaoUtil();
     }
 
+    public double getBonificacao() {
+        return super.getSalario();
+    }
+
+    @Override
+    public void setSenha(int senha) {
+        this.autenticador.setSenha(senha);
+
+    }
+
+    @Override
+    public boolean autentica(int senha) {
+        return this.autenticador.autentica(senha);
+    }
 }
