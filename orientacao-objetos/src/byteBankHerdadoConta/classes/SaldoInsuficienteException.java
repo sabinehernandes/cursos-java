68e7f9d22e3fde7f350ba7bbad2a37928d9930b2
Sabine Hernandes <sabinehernandes@gmail.com>
2021-06-24 16:42:20 -0300

    Criação da classe para implementação dos conceitos de tratamento de exceção

---
+++ orientacao-objetos/src/byteBankHerdadoConta/classes/SaldoInsuficienteException.java
@@ -0,0 +1,8 @@
+package byteBankHerdadoConta.classes;
+
+public class SaldoInsuficienteException extends Exception {
+
+    public SaldoInsuficienteException(String mensagem) {
+        super(mensagem);
+    }
+}
