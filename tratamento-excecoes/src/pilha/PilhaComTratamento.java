c032f2164caec57f7ae20b4792e8465f5fe61130
Sabine Hernandes <sabinehernandes@gmail.com>
2021-06-24 10:27:05 -0300

    Aplicando conceitos de try catch

---
+++ tratamento-excecoes/src/pilha/PilhaComTratamento.java
@@ -0,0 +1,34 @@
+package pilha;
+
+public class PilhaComTratamento {
+
+    public static void main(String[] args) {
+        System.out.println("Ini do main");
+        try {
+            metodo1();
+            // O | equivale a fazer outro try catch do NullPointer
+        } catch (ArithmeticException | NullPointerException ex) {
+            String mensagem = ex.getMessage();
+            System.out.println("Exception " + mensagem);
+            ex.printStackTrace();
+        }
+        System.out.println("Fim do main");
+    }
+
+    private static void metodo1() {
+        System.out.println("Ini do metodo1");
+            metodo2();
+        System.out.println("Fim do metodo1");
+    }
+
+    private static void metodo2() {
+        System.out.println("Ini do metodo2");
+        for(int i = 1; i <= 5; i++) {
+            System.out.println(i);
+            //int a = i / 0;
+            Conta conta = null;
+            conta.deposita();
+        }
+        System.out.println("Fim do metodo2");
+    }
+}
